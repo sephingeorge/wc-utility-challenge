6061709fc744b8bba06a3c151b314efd0ceddfbc
sephingeorge <contactmesephin@gmail.com>
2024-08-12 11:43:55 +0530

    F2: Changes added

--- src/main/java/challenge/coding/wc/exception/NewClass1.java
+++ src/main/java/challenge/coding/wc/exception/NewClass1.java
@@ -3,7 +3,7 @@ package challenge.coding.wc.exception;
 public class NewClass1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello this is Sephin");
     }
 
     private static void display() {
